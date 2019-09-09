33086cc14df4c7595220934e599fd52c5160e5f6
Andrii Olkhovych <bronzemonet@gmail.com>
2019-09-09 15:15:25 +0300

    Thanks Samuel James for teaching me).

--- js/main.js
+++
@@ -1,9 +0,0 @@
-Vue.component('input-textbox',
-{
-  props: [],
-  template: '<div id="app">'+
-              '<p>{{ message }}</p>'+
-              '<input v-model="message"/>'+
-            '</div>'
-})
-  
---
+++ public/src/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import Vue from 'vue'
+import './css/bootstrap.min.css';
+import App from './components/App.vue'
+
+new Vue({
+  el: 'app',
+  components: {App},
+  methods: {}
+})
