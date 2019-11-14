45da200581f63d5e41954cd4287754a8a58a2000
Def4alt <bronzemonet@gmail.com>
2019-11-14 22:35:46 +0200

    fix

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { withTheme } from "../Theme";
 import { withAuthentication } from "../Session";
-import { withLocalization, useLocalization } from "../Localization";
+import { withLocalization, useLocalization, stringsType } from "../Localization";
 import { CookiesProvider } from "react-cookie";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import * as ROUTES from "../../Constants/routes";
@@ -11,12 +11,14 @@ import SignUpPage from "../SignUp";
 import BugsPage from "../Bugs";
 import SignInPage from "../SignIn";
 import PasswordForgetPage from "../PasswordForget";
+import LandingPage from "../Landing";
 import HomePage from "../Home";
 import AccountPage from "../Account";
 import AdminPage from "../Admin";
 
-
-interface AppProps {}
+interface AppProps {
+	strings: stringsType
+}
 
 const App: React.FunctionComponent<AppProps> = props => (
 	<CookiesProvider>
@@ -24,15 +26,31 @@ const App: React.FunctionComponent<AppProps> = props => (
 			<div className="root">
 				<Navigation />
 				<div className="base">
-					<Route exact path={ROUTES.LANDING} component={SignInPage} />
+					<Route
+						render={({ history }) => (
+							<div className="foundBug">
+								<button
+									onClick={() => history.push(ROUTES.BUGS)}
+								>
+									{props.strings.foundBug}
+								</button>
+							</div>
+						)}
+					/>
+
+					<Route
+						exact
+						path={ROUTES.LANDING}
+						component={LandingPage}
+					/>
 					<Route path={ROUTES.SIGN_UP} component={SignUpPage} />
 					<Route path={ROUTES.SIGN_IN} component={SignInPage} />
+					<Route path={ROUTES.BUGS} component={BugsPage} />
 					<Route
 						path={ROUTES.PASSWORD_FORGET}
 						component={PasswordForgetPage}
 					/>
 					<Route path={ROUTES.HOME} component={HomePage} />
-					<Route path={ROUTES.BUGS} component={BugsPage} />
 					<Route path={ROUTES.ACCOUNT} component={AccountPage} />
 					<Route path={ROUTES.ADMIN} component={AdminPage} />
 				</div>
