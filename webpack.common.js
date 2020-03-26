a397cebf2bd6c4060c7d69f465a15cd35c90b7f7
Andrii <bronzemonet@gmail.com>
2020-03-26 19:42:01 +0200

    webpack and react

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const { CheckerPlugin } = require ("awesome-typescript-loader");
+const HtmlWebpackPlugin = require ("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  resolve: {
+    extensions: [".ts", ".tsx", ".json", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        include: /src/,
+        test: /\.(ts|tsx)$/,
+        use: ["babel-loader", "awesome-typescript-loader"]
+      },
+      {
+        test: /\.(jpeg|png|svg)$/i,
+        loader: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new CheckerPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html")
+    })
+  ]
+};
---
+++ webpack.dev.js
@@ -0,0 +1,25 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = merge(common, {
+  mode: "development",
+  devServer: {
+    hot: true // enable HMR on the server
+  },
+  devtool: "cheap-module-eval-source-map",
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
+    new webpack.NamedModulesPlugin() // prints more readable module names in the browser console on HMR updates
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+});
