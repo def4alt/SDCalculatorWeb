7080a6971d0fb503bf6eff4175d7842bcc3ecda2
Andrii <bronzemonet@gmail.com>
2020-03-30 16:18:13 +0300

    ci fix #6

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common");
 const webpack = require("webpack");
-const Dotenv = require("dotenv-webpack");
 
 module.exports = merge(common, {
     mode: "development",
