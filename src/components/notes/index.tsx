ad4eb01867b1d8097c5a3eb072e4876239d2a286
Andrii Olkhovych <bronzemonet@gmail.com>
2021-01-31 15:24:44 +0200

    Notes submit reload fix

--- src/components/notes/index.tsx
+++ src/components/notes/index.tsx
@@ -113,7 +113,7 @@ const Notes: React.FC<NotesProps> = (props) => {
             >
                 <GoNote/>
             </button>
-            <form className="notes__form notes__form_elevated" ref={floatingNotesRef}>
+            <form className="notes__form notes__form_elevated" ref={floatingNotesRef} onSubmit={onSubmit}>
                 <p className="notes__title">{localization.methodName}</p>
                 <input
                     className="notes__input"
