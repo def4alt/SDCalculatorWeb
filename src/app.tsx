d5b55e2606d3f6751f2dd72dc4dad235893e4aca
Andrii Olkhovych <bronzemonet@gmail.com>
2021-07-25 10:32:01 -0700

    Optimize manifest and html file to PWA

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route } from "react-router-dom";
 import * as ROUTES from "./routes";
-import About from "./components/about";
-import Home from "./components/home";
-import Admin from "./components/admin";
-import SignUp from "./components/sign_up";
-import SignIn from "./components/sing_in";
-import Account from "./components/account";
-import Navigation from "./components/navigation";
-import Settings from "./components/settings";
-import PasswordForget from "./components/password_forget";
+import About from "Components/about";
+import Home from "Components/home";
+import Admin from "Components/admin";
+import SignUp from "Components/sign_up";
+import SignIn from "Components/sing_in";
+import Account from "Components/account";
+import Navigation from "Components/navigation";
+import Settings from "Components/settings";
+import PasswordForget from "Components/password_forget";
 import { withAuthentication } from "./context/session";
 import { withLocalization } from "./context/localization";
 import withFirebase from "./context/firebase/withFirebase";
 import { BrowserRouter as Router } from "react-router-dom";
+import Loading from "Components/loading";
 
 const App: React.FC = (_) => (
     <Router>
         <div className="root">
-            <Navigation />
+            <Suspense fallback={<Loading />}>
+                <Navigation />
+            </Suspense>
             <Route exact path={ROUTES.HOME} component={Home} />
             <Route path={ROUTES.ABOUT} component={About} />
             <Route path={ROUTES.SIGN_UP} component={SignUp} />
