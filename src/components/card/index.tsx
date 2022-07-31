56df17b5c3428005f750f4ddc36c9841b0241fd0
Andrii Olkhovych <the.def4alt@gmail.com>
2022-07-31 17:44:52 +0200

    Switch to Chart.js

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,48 +1,26 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import { StatModel } from "../../types";
 import { MdAdd, MdClear } from "react-icons/md";
-import LineChart from "Components/line_chart";
+import { Chart } from "Components/chart";
 
 import "Styles/card/card.scss";
+import moment from "moment";
 
 interface CardProps {
     model: StatModel;
-    width: number;
     showSDCV: boolean;
 }
 
 const Card: React.FC<CardProps> = (props) => {
-    const [inView, setInView] = useState<boolean>(false);
     const [hideFromPrint, setHideFromPrint] = useState<boolean>(false);
 
     const cardRef = useRef<HTMLDivElement>(null);
-
-    const isInView = () => {
-        const card = cardRef.current;
-
-        if (!card) return false;
-
-        const rect = card.getBoundingClientRect();
-
-        setInView(
-            rect.top >= -300 &&
-                rect.bottom <=
-                    (window.innerHeight + 300 ||
-                        document.documentElement.clientHeight + 300)
-        );
-    };
+    const model = props.model;
 
     const hasWarning = useMemo(
-        () => props.model.Warnings.filter((t) => t.trim() !== "").length > 0,
-        [props.model.Warnings, props.model.Warnings.length]
+        () => model.Warnings.filter((t) => t.trim() !== "").length > 0,
+        [model.Warnings, props.model.Warnings.length]
     );
-
-    useEffect(() => {
-        window.addEventListener("scroll", isInView);
-        isInView();
-
-        return () => window.removeEventListener("scroll", isInView);
-    }, []);
 
     const cardState = useMemo(() => {
         if (hideFromPrint) return "card card_hidden";
@@ -52,25 +30,40 @@ const Card: React.FC<CardProps> = (props) => {
         return "card";
     }, [hideFromPrint, hasWarning]);
 
-    const sd = useMemo(
-        () => Math.floor(props.model.SD * 100) / 100,
-        [props.model.SD]
-    );
+    const sd = useMemo(() => Math.floor(model.SD * 100) / 100, [model.SD]);
 
     const cv = useMemo(
-        () =>
-            Math.floor((props.model.SD / props.model.Average[0]) * 100 * 100) /
-            100,
-        [props.model.Average]
+        () => Math.floor((model.SD / model.Average[0]) * 100 * 100) / 100,
+        [model.Average]
     );
 
+    const labels = useMemo(
+        () =>
+            [...Array(model.Average.length)].map(
+                (_, i) =>
+                    moment(model.Date.at(i))
+                        .format("DD/MM/YY")
+                        .toLocaleString() +
+                    ";" +
+                    model.Warnings.at(i)
+            ),
+        [model.Average.length, model.Warnings]
+    );
+
+    const data = useMemo(() => {
+        return {
+            values: model.Average,
+            average: model.Average[0],
+            sd: model.SD,
+            labels,
+        };
+    }, [model.Average.length, model.SD, labels.length]);
+
     return (
-        <div className={cardState} ref={cardRef} style={{ width: props.width }}>
+        <div className={cardState} ref={cardRef}>
             <div className="card__header">
                 <p className="card__title">
-                    {props.model.TestName +
-                        " Lvl" +
-                        String(props.model.SampleType)}
+                    {model.TestName + " Lvl" + String(model.SampleType)}
                 </p>
                 <button
                     className="card__close"
@@ -79,8 +72,8 @@ const Card: React.FC<CardProps> = (props) => {
                     {hideFromPrint ? <MdAdd /> : <MdClear />}
                 </button>
             </div>
-            <div className={inView ? "card__chart" : "card__chart_hidden"}>
-                <LineChart model={props.model} width={props.width} />
+            <div className="card__chart">
+                <Chart data={data} />
             </div>
             <div
                 className={
---
+++ src/components/chart/index.tsx
@@ -0,0 +1,146 @@
+import React, { useEffect, useMemo, useState } from "react";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    ChartOptions,
+    CoreScaleOptions,
+    Scale,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { ChartJSOrUndefined } from "react-chartjs-2/dist/types";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement);
+
+export const Chart: React.FC<{
+    data: {
+        values: number[];
+        labels: string[];
+        average: number;
+        sd: number;
+    };
+}> = ({ data }) => {
+    const { values, labels, average, sd } = data;
+    const [reference, setReference] = useState<ChartJSOrUndefined<
+        "line",
+        number[],
+        string
+    > | null>(null);
+
+    const chartData = {
+        labels,
+        datasets: [
+            {
+                label: "Value",
+                data: values,
+                backgroundColor: "#d63031",
+                borderColor: "#d63031",
+            },
+        ],
+    };
+
+    useEffect(() => {
+        if (!reference) return;
+        if (!reference.data.labels) return;
+
+        reference.data.datasets[0].data = values;
+        reference.data.labels = labels;
+        reference.update();
+    });
+
+    const options: ChartOptions<"line"> = useMemo(() => {
+        return {
+            responsive: true,
+            animation: false,
+            maintainAspectRatio: false,
+            datasets: {
+                line: {
+                    borderWidth: 4,
+                },
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        minRotation: 0,
+                        maxRotation: 90,
+                        callback(
+                            this: Scale<CoreScaleOptions>,
+                            tickValue: string | number
+                        ) {
+                            const value = Number(tickValue);
+                            const label = this.getLabelForValue(value); // eslint-disable-line react/no-this-in-sfc
+                            return label.split(";")[0];
+                        },
+                    },
+                    grid: {
+                        display: false,
+                    },
+                },
+                xAxisViolations: {
+                    type: "category",
+                    grid: { drawOnChartArea: false },
+                    ticks: {
+                        callback(
+                            this: Scale<CoreScaleOptions>,
+                            tickValue: string | number
+                        ) {
+                            const value = Number(tickValue);
+                            const label = this.getLabelForValue(value); // eslint-disable-line react/no-this-in-sfc
+                            return label.split(";")[1];
+                        },
+                        color: "#c62828",
+                        font: {
+                            size: 15,
+                        },
+                    },
+                },
+                y: {
+                    min: average - 4 * sd,
+                    max: average + 4 * sd,
+                    ticks: {
+                        stepSize: sd,
+                        callback: (tickValue: string | number) => {
+                            const value = Number(tickValue);
+                            if (value === average) {
+                                return Math.floor(value * 100) / 100 + ", M";
+                            }
+                            return (
+                                Math.floor(value * 100) / 100 +
+                                ", " +
+                                Math.floor(Math.abs(value - average) / sd) +
+                                "SD"
+                            );
+                        },
+                    },
+
+                    grid: {
+                        drawBorder: false,
+                        color: [
+                            "#fffff",
+                            "#e84393",
+                            "#00cec9",
+                            "#ff7675",
+                            "#6c5ce7",
+                            "#ff7675",
+                            "#00cec9",
+                            "#e84393",
+                            "#fffff",
+                        ],
+                        lineWidth: 1.5,
+                        borderDash: [8, 4],
+                    },
+                },
+            },
+        };
+    }, [average, sd]);
+
+    return (
+        <Line
+            options={options}
+            data={chartData}
+            ref={(reference) => setReference(reference)}
+        />
+    );
+};
